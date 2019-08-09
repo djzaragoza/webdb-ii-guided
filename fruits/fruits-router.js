d236bd344dd37c3078b9e74504808af36ba1d25c
unknown <dj.zaragoza@icloud.com>
2019-08-08 22:04:16 -0700

    working on fruits-router.js file

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const knex = require('knex');
+
+// compare this to the original code in the guided repo
+// this is the "best practice" way to do it ... keep your db config out
+// of your express router code.
 
 const db = require('../data/db-config.js');
 
@@ -8,9 +11,9 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const fruits = await db('fruits');
-    res.json(fruits); 
+    res.json(fruits);
   } catch (err) {
-    res.status(500).json({ message: 'Failed to retrieve fruits' });
+    res.status(500).json({ message: 'Failed to retrieve fruits'});
   }
 });
 
@@ -18,24 +21,10 @@ router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const fruit = await db('fruits').where({ id });
-    
+
     res.json(fruit);
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to retrieve fruit' });
+  } catch(err) {
+    res.status(500).json({ message: 'Failed to retrieve fruit '});
   }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const fruitData = req.body;
-    const [ id ] = await db('fruits').insert(fruitData);
-    const newFruitEntry = await db('fruits').where({ id });
-  
-    res.status(201).json(newFruitEntry);
-  } catch (err) {
-    console.log('POST error', err);
-    res.status(500).json({ message: "Failed to store data" });
-  }
-});
-
-module.exports = router;
