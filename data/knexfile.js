e3e1388cf48715d96277b8255f4685d72ce36f8f
unknown <dj.zaragoza@icloud.com>
2019-08-08 21:54:51 -0700

    finished knexfile.js

--- data/knexfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    development: {
-        client: 'sqlite3',
-        connection: {
-            filename: './produce.db3'
-        },
-        useNullAsDefault: true
-    };
-};
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,20 @@
 // Update with your config settings.
+// be sure you are in the root of your repo! or use --knexfile ...
+
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+      filename: './data/produce.db3'
+    },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './data/seeds'
     }
   }
-
 };
